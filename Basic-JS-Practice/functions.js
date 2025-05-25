ff432d472df1f76d8161dd2def1367f02164e58f
Parisa Mohammadzadeh <110891110+shahpari2kht@users.noreply.github.com>
2025-05-25 08:20:39 +0330

    Create functions.js

---
+++ Basic-JS-Practice/functions.js
@@ -0,0 +1,17 @@
+// تابع جمع دو عدد
+function sum(a, b) {
+    return a + b;
+}
+
+// تابع خوش‌آمدگویی ساده
+function sayHello() {
+    console.log('Hello, welcome!');
+}
+
+var c = sum(1, 3);
+console.log('sum(1, 3) =', c);
+
+c = sum(16, 5);
+console.log('sum(16, 5) =', c);
+
+sayHello();
