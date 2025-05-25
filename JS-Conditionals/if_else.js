4e2d966bbf999f101d5a4ebe5a333f69eb99fdff
Parisa Mohammadzadeh <110891110+shahpari2kht@users.noreply.github.com>
2025-05-25 08:26:08 +0330

    Update if_else.js

--- JS-Conditionals/if_else.js
+++ JS-Conditionals/if_else.js
@@ -1,1 +1,26 @@
+// تمرین if/else : بررسی سن برای تعیین گروه سنی
+
+let age = 21;
+
+if (age < 0) {
+    console.log("سن نامعتبر است.");
+} else if (age < 13) {
+    console.log("کودک");
+} else if (age < 20) {
+    console.log("نوجوان");
+} else if (age < 40) {
+    console.log("جوان");
+} else if (age < 60) {
+    console.log("میانسال");
+} else {
+    console.log("سالمند");
+}
+
+// تمرین منطقی دیگر: بررسی زوج یا فرد بودن عدد
+let number = 17;
+if (number % 2 === 0) {
+    console.log(number + " عددی زوج است.");
+} else {
+    console.log(number + " عددی فرد است.");
+}
 
