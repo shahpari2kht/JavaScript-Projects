46a3b94853c35b7f382462d1e106ba4b954f16b9
Parisa Mohammadzadeh <110891110+shahpari2kht@users.noreply.github.com>
2025-05-25 08:21:07 +0330

    Create conditions.js

---
+++ Basic-JS-Practice/conditions.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var a = 50;
+var b = 8;
+
+if (a == 5) {
+    console.log('print 5');
+} else if (b == 6) {
+    console.log('print 6');
+} else if (a == 7) {
+    console.log('print 7');
+} else {
+    console.log('it's wrong');
+}
